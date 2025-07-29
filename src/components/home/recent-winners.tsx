03855ccf9ed2548271384c838e15609614bcb195
Dia De Roça <diaderoca1@gmail.com>
2025-07-29 12:23:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/home/recent-winners.tsx
+++ src/components/home/recent-winners.tsx
@@ -1,47 +1,70 @@
+"use client";
+
 import Image from "next/image";
 import { recentWinners } from "@/lib/data";
 import { Card, CardContent } from "@/components/ui/card";
 import { Dot } from "lucide-react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 
 export default function RecentWinners() {
-    const winnersToDisplay = recentWinners.slice(0, 2);
   return (
     <div className="flex items-center gap-4">
-        <div className="flex items-center text-primary font-bold">
-            <Dot className="size-10 animate-ping" />
-            <span>AO VIVO</span>
-        </div>
-      <div className="grid grid-cols-2 gap-4 flex-1">
-        {winnersToDisplay.map((winner, index) => (
-          <Card key={index} className="bg-secondary border-none">
-            <CardContent className="p-3 flex items-center gap-3">
-              <div className="relative size-12 flex-shrink-0">
-                <Image
-                  src={winner.imageUrl}
-                  alt={winner.prizeName}
-                  fill
-                  className="object-contain"
-                  data-ai-hint={winner.aiHint}
-                />
-              </div>
-              <div className="flex flex-col text-sm overflow-hidden">
-                <span className="font-medium text-amber-400/95 overflow-hidden text-nowrap text-ellipsis">
-                  {winner.winnerName}
-                </span>
-                <h1 className="font-medium text-muted-foreground overflow-hidden text-nowrap text-ellipsis">
-                  {winner.prizeName}
-                </h1>
-                <span className="font-semibold">
-                  <span className="text-primary">R$ </span>
-                  {winner.prizeValue.toLocaleString("pt-BR", {
-                    minimumFractionDigits: 2,
-                  })}
-                </span>
-              </div>
-            </CardContent>
-          </Card>
-        ))}
+      <div className="flex items-center text-primary font-bold">
+        <Dot className="size-10 animate-ping" />
+        <span>AO VIVO</span>
       </div>
+      <Carousel
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+        plugins={[
+          Autoplay({
+            delay: 2000,
+            stopOnInteraction: false,
+          }),
+        ]}
+        className="w-full"
+      >
+        <CarouselContent>
+          {recentWinners.map((winner, index) => (
+            <CarouselItem key={index} className="basis-1/2 md:basis-1/2">
+               <Card className="bg-secondary border-none">
+                <CardContent className="p-3 flex items-center gap-3">
+                  <div className="relative size-12 flex-shrink-0">
+                    <Image
+                      src={winner.imageUrl}
+                      alt={winner.prizeName}
+                      fill
+                      className="object-contain"
+                      data-ai-hint={winner.aiHint}
+                    />
+                  </div>
+                  <div className="flex flex-col text-sm overflow-hidden">
+                    <span className="font-medium text-amber-400/95 overflow-hidden text-nowrap text-ellipsis">
+                      {winner.winnerName}
+                    </span>
+                    <h1 className="font-medium text-muted-foreground overflow-hidden text-nowrap text-ellipsis">
+                      {winner.prizeName}
+                    </h1>
+                    <span className="font-semibold">
+                      <span className="text-primary">R$ </span>
+                      {winner.prizeValue.toLocaleString("pt-BR", {
+                        minimumFractionDigits: 2,
+                      })}
+                    </span>
+                  </div>
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </div>
   );
 }
