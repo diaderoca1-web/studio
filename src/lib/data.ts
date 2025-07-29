253273eb3a200b954e97bea95bee16040941de8f
Dia De Roça <diaderoca1@gmail.com>
2025-07-29 13:33:35 +0000

    coloque as imagens nos respectivos lugares cada link esta renomeado. col

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,4 @@
+
 export type ScratchCardType = {
   slug: string;
   title: string;
@@ -22,10 +23,18 @@ export const heroBanners = [
 ];
 
 export const recentWinners = [
-    { winnerName: "Léia Pr***", prizeName: "Caixa de som JB...", prizeValue: 2500.00, imageUrl: "https://placehold.co/80x80.png", aiHint: "bluetooth speaker" },
-    { winnerName: "Pietra Pe**", prizeName: "Datejust 41", prizeValue: 75400.00, imageUrl: "https://placehold.co/80x80.png", aiHint: "luxury watch" },
-    { winnerName: "Wilson C.", prizeName: "iPhone 15 Pro", prizeValue: 11000.00, imageUrl: "https://placehold.co/80x80.png", aiHint: "smartphone" },
-    { winnerName: "Eloá D.", prizeName: "Smart TV 4K 55\"", prizeValue: 3000.00, imageUrl: "https://placehold.co/80x80.png", aiHint: "television screen" },
+    { winnerName: "Léia Pr***", prizeName: "Caixa de som JBL", prizeValue: 2500.00, imageUrl: "https://ik.imagekit.io/azx3nlpdu/variant_jbl_boombox_3_black.png?updatedAt=1751634894498", aiHint: "bluetooth speaker" },
+    { winnerName: "Pietra Pe**", prizeName: "iPhone 15 Pro Max", prizeValue: 11000.00, imageUrl: "https://ik.imagekit.io/azx3nlpdu/variant_iphone_15_pro_max_256_gb_nio_preto.png?updatedAt=1751634894448", aiHint: "smartphone" },
+    { winnerName: "Wilson C.", prizeName: "Moto Pop 110i", prizeValue: 9500.00, imageUrl: "https://ik.imagekit.io/azx3nlpdu/variant_pop_110i_branco.png?updatedAt=1751634894490", aiHint: "scooter" },
+    { winnerName: "Eloá D.", prizeName: "iPhone 15 Pro", prizeValue: 11000.00, imageUrl: "https://ik.imagekit.io/azx3nlpdu/variant_iphone_15_pro_256_gb_tit_nio_natural.png?updatedAt=1751634894188", aiHint: "smartphone" },
+    { winnerName: "Carlos F.", prizeName: "Galaxy Z Flip5", prizeValue: 6000.00, imageUrl: "https://ik.imagekit.io/azx3nlpdu/variant_galaxy_z_flip5_256_gb_creme.png?updatedAt=1751634892797", aiHint: "smartphone" },
+    { winnerName: "Ana G.", prizeName: "Motorola Edge 40 Neo", prizeValue: 2500.00, imageUrl: "https://ik.imagekit.io/azx3nlpdu/variant_edge_40_neo_256_gb_black_beauty.png?updatedAt=1751634892779", aiHint: "smartphone" },
+    { winnerName: "Beatriz L.", prizeName: "Moto Biz 110i", prizeValue: 13000.00, imageUrl: "https://ik.imagekit.io/azx3nlpdu/variant_biz_110i_vermelho.png?updatedAt=1751634892737", aiHint: "scooter" },
+    { winnerName: "Daniel M.", prizeName: "Xbox Series X", prizeValue: 4500.00, imageUrl: "https://ik.imagekit.io/azx3nlpdu/item_xbox_series_x.png?updatedAt=1751634892410", aiHint: "video game console" },
+    { winnerName: "Eduarda N.", prizeName: "Sofá 3 Lugares", prizeValue: 1800.00, imageUrl: "https://ik.imagekit.io/azx3nlpdu/item_sof_3_lugares_beny_base.png?updatedAt=1751634892378", aiHint: "sofa" },
+    { winnerName: "Fernanda P.", prizeName: "Smartwatch D20", prizeValue: 150.00, imageUrl: "https://ik.imagekit.io/azx3nlpdu/item_smartwatch_d20_shock.png?updatedAt=1751634892443", aiHint: "smartwatch" },
+    { winnerName: "Gustavo Q.", prizeName: "Smart TV 4K 55\"", prizeValue: 3000.00, imageUrl: "https://ik.imagekit.io/azx3nlpdu/item_smart_tv_4k_55.png?updatedAt=1751634892461", aiHint: "television screen" },
+    { winnerName: "Heitor R.", prizeName: "Playstation 5", prizeValue: 4500.00, imageUrl: "https://ik.imagekit.io/azx3nlpdu/item_playstation_5.png?updatedAt=1751634892317", aiHint: "video game console" },
 ];
 
 export const scratchCards: ScratchCardType[] = [
