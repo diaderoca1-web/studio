2d0eb75f238293dd452d307e7d8c160d8633d5b4
Dia De Roça <diaderoca1@gmail.com>
2025-08-01 06:08:04 +0000

    ta na pasta mano é essa kkkk usa ela ai pow

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,3 +1,4 @@
+
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
@@ -5,7 +6,7 @@ export default function Logo({ className, ...props }: { className?: string }) {
     return (
         <div className={cn("relative", className)} {...props}>
             <Image
-                src="https://i.ibb.co/3kC2Vv4/tomate-seco-e-raspadinha-uma-variedade-para-voce-escolher-sushi-e-macarrao.png"
+                src="/images/logo.png"
                 alt="Raspadinha.click Logo"
                 fill
                 className="object-contain"
