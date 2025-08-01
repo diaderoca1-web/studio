159219afda82cf1054d3a8c47220d80092926aad
Dia De Roça <diaderoca1@gmail.com>
2025-08-01 06:05:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,12 +1,15 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import logoImage from "/images/tomate-seco-e raspadinha-uma variedade-para-voce-escolher-sushi-e-macarrao.png";
 
 export default function Logo({ className, ...props }: { className?: string }) {
     return (
         <div className={cn("relative", className)} {...props}>
-            <img
-                src="https://i.ibb.co/6gZ3GAX/raspadinha-click-logo.png"
+            <Image
+                src={logoImage}
                 alt="Raspadinha.click Logo"
                 className="object-contain h-full w-full"
+                unoptimized
             />
         </div>
     );
