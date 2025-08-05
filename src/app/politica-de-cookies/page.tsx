b94d0c32286b8e187e672a7e1e37ba6e46ff8505
Dia De Roça <diaderoca1@gmail.com>
2025-08-05 23:40:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/politica-de-cookies/page.tsx
+++ src/app/politica-de-cookies/page.tsx
@@ -23,13 +23,13 @@ export default function CookiePolicyPage() {
 
             <h2>Desativar cookies</h2>
             <p>
-                Você pode impedir a configuração de cookies ajustando as configurações do seu navegador (consulte a Ajuda do seu navegador para saber como fazer isso). Esteja ciente de que a desativação de cookies afetará a funcionalidade deste и de muitos outros sites que você visita. A desativação de cookies geralmente resultará na desativação de certas funcionalidades e recursos deste site. Portanto, é recomendável que você não desative os cookies.
+                Você pode impedir a configuração de cookies ajustando as configurações do seu navegador (consulte a Ajuda do seu navegador para saber como fazer isso). Esteja ciente de que a desativação de cookies afetará a funcionalidade deste e de muitos outros sites que você visita. A desativação de cookies geralmente resultará na desativação de certas funcionalidades e recursos deste site. Portanto, é recomendável que você não desative os cookies.
             </p>
 
             <h2>Cookies que definimos</h2>
             <ul>
                 <li>
-                    <strong>Cookies relacionados à conta:</strong> Se você criar uma conta conosco, usaremos cookies para o gerenciamento do processo de inscrição e administração geral. Esses cookies geralmente serão excluídos quando você sair do sistema, porém, em alguns casos, eles poderão permanecer posteriormente para lembrar as preferências do seu site ao sair.
+                    <strong>Cookies relacionados à conta:</strong> If you create an account conosco, usaremos cookies para o gerenciamento do processo de inscrição e administração geral. Esses cookies geralmente serão excluídos quando você sair do sistema, porém, em alguns casos, eles poderão permanecer posteriormente para lembrar as preferências do seu site ao sair.
                 </li>
                 <li>
                     <strong>Cookies relacionados ao login:</strong> Utilizamos cookies quando você está logado, para que possamos lembrar dessa ação. Isso evita que você precise fazer login sempre que visitar uma nova página. Esses cookies são normalmente removidos ou limpos quando você efetua logout para garantir que você possa acessar apenas a recursos e áreas restritas ao efetuar login.
@@ -52,7 +52,7 @@ export default function CookiePolicyPage() {
             <h2>Mais informações</h2>
             <p>
                 Esperamos que esteja esclarecido e, como mencionado anteriormente, se houver algo que você não tem certeza se precisa ou não, geralmente é mais seguro deixar os cookies ativados, caso interaja com um dos recursos que você usa em nosso site.
-            </grup>
+            </p>
         </CardContent>
       </Card>
     </div>
